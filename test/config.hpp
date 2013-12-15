2a2aca8fdeca4d70d00a99715416bbe9ef4033db
rhysd <lin90162@yahoo.co.jp>
2013-12-15 20:38:35 +0900

    add test config header and empty test case

---
+++ test/config.hpp
@@ -0,0 +1,11 @@
+#if !defined CONFIG_HPP_INCLUDED
+#define      CONFIG_HPP_INCLUDED
+
+#include "../include/array_view.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+
+#endif    // CONFIG_HPP_INCLUDED
---
+++ test/test_ctors.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE CtorsTest
+#include "./config.hpp"
+
+BOOST_AUTO_TEST_SUITE(ctors)
+
+BOOST_AUTO_TEST_CASE(constructors) {
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END()
