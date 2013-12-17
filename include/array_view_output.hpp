26a212144a91c972ea39467cc88d80dded32fa5d
rhysd <lin90162@yahoo.co.jp>
2013-12-18 02:28:08 +0900

    make array_view outputtable

---
+++ include/array_view_output.hpp
@@ -0,0 +1,31 @@
+#if !defined ARV_ARRAY_VIEW_OUTPUT_HPP_INCLUDED
+#define      ARV_ARRAY_VIEW_OUTPUT_HPP_INCLUDED
+
+#include <ostream>
+
+namespace arv {
+template<class T>
+class array_view;
+} // namespace arv
+
+template<class T>
+std::ostream &operator<<(std::ostream &ost, arv::array_view<T> const& av)
+{
+    ost << '{';
+
+    auto itr = std::begin(av);
+    auto const end = std::end(av);
+    while(true) {
+        ost << *itr;
+        if(++itr != end) {
+            ost << ", ";
+        } else {
+            break;
+        }
+    }
+
+    ost << '}';
+    return ost;
+}
+
+#endif    // ARV_ARRAY_VIEW_OUTPUT_HPP_INCLUDED
