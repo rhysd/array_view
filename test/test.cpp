2342332c638e6ce7847cfbacea58853fa56e7c9b
rhysd <lin90162@yahoo.co.jp>
2013-12-20 03:06:32 +0900

    unify to one file

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,14 @@
 #define BOOST_TEST_MODULE ArrayViewTest
-#include "./config.hpp"
+
+#include "../include/array_view.hpp"
+
+using arv::array_view;
+using arv::make_view;
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(array_view_test)
 
